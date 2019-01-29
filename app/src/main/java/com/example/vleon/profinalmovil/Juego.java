3d789228f923efb05cab62033a8be3541cf69ebf
VLeonBar <vleonbarciela@gmail.com>
2019-01-29 20:21:04 +0100

    -

--- app/src/main/java/com/example/vleon/profinalmovil/Juego.java
+++ app/src/main/java/com/example/vleon/profinalmovil/Juego.java
@@ -13,19 +13,22 @@ import java.util.ArrayList;
 public class Juego extends Escena {
     ArrayList<Ficha> fichasJuego = new ArrayList<>();
     Bitmap imgGradaIzq, imgGradaDch, imgLogo;
-    int anchoDecimo, altoMedio, anchoTercio, altoSexto, anchoMedio;
+    int anchoDecimo, altoMedio, anchoQuinto, altoSexto, anchoMedio;
+    int ancho_1 = 1, ancho_2 = 2;
     float posCentroFichaX, posCentroFichaY;
     Ficha ficha;
+    Rect[] casillas = new Rect[9];
 
     public Juego(Context contexto, int idEscena, int anchoPantalla, int altoPantalla) {
         super(contexto, idEscena, anchoPantalla, altoPantalla);
         imgFondo = BitmapFactory.decodeResource(contexto.getResources(), R.drawable.fondo_game_edit);
         imgFondo = Bitmap.createScaledBitmap(imgFondo, anchoPantalla, altoPantalla, false);
         anchoDecimo = anchoPantalla / 10;
-        anchoTercio = anchoPantalla / 3;
+        anchoQuinto = anchoPantalla / 5;
         anchoMedio = anchoPantalla / 2;
         altoMedio = altoPantalla / 2;
         altoSexto = altoMedio / 3;
+
         //Ficha
         ficha = new Ficha(0, 0, new BitmapFactory().decodeResource(contexto.getResources(), R.drawable.bandera9));
         ficha.setImgFicha(Bitmap.createScaledBitmap(ficha.getImgFicha(), altoSexto / 2, altoSexto / 2, false));
@@ -33,12 +36,20 @@ public class Juego extends Escena {
         ficha.setPosY(altoSexto * (float) 5.5 - ficha.getImgFicha().getHeight() / 2);
         posCentroFichaX = ficha.getPosX() + ficha.getImgFicha().getWidth() / 2;
         posCentroFichaY = ficha.getPosY() + ficha.getImgFicha().getHeight() / 2;
+        //Gradas e icono central
         imgLogo = BitmapFactory.decodeResource(contexto.getResources(), R.drawable.icono);
         imgLogo = Bitmap.createScaledBitmap(imgLogo, anchoDecimo * 6, altoPantalla / 3, false);
         imgGradaDch = BitmapFactory.decodeResource(contexto.getResources(), R.drawable.gradadch_main);
         imgGradaDch = Bitmap.createScaledBitmap(imgGradaDch, anchoDecimo, altoMedio, false);
         imgGradaIzq = BitmapFactory.decodeResource(contexto.getResources(), R.drawable.gradaizq_main);
         imgGradaIzq = Bitmap.createScaledBitmap(imgGradaIzq, anchoDecimo, altoMedio, false);
+        //Rectángulos - Botones
+
+        for (int i = 0; i < casillas.length; i++) {
+            casillas[i] = new Rect(anchoQuinto * ancho_1, altoSexto * ancho_1, anchoQuinto * ancho_2, altoSexto * ancho_2);
+            ancho_1++;
+            ancho_2++;
+        }
     }
 
     public int actualizarFisica() {
@@ -53,6 +64,9 @@ public class Juego extends Escena {
             c.drawBitmap(imgLogo, anchoDecimo * 2, altoMedio, null);
             c.drawBitmap(imgGradaDch, anchoDecimo * 9, altoMedio, null);
             c.drawBitmap(imgGradaIzq, 0, altoMedio, null);
+            for (int i = 0; i < casillas.length; i++) {
+               c.drawRect(casillas[i],pincel);
+            }
             c.drawLine(0, 0, anchoPantalla, 0, pincel);
             c.drawLine(0, altoSexto * 3, anchoPantalla, altoSexto * 3, pincel);
             c.drawCircle(anchoMedio, altoSexto * (float) 5.5, altoSexto / (float) 2.5, pincel);
